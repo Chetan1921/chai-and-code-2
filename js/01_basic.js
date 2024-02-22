669dbd619c3b2b9455c96bfe44f9842c90435f1d
Chetan1921 <149550764+Chetan1921@users.noreply.github.com>
2024-02-22 09:52:54 +0000

    conversion of data type

---
+++ js/01_basic.js
@@ -0,0 +1,15 @@
+console.log("chetan");
+let name="sandeep"
+let age=22;
+let islog=false
+console.log(typeof name)
+console.log(typeof null)
+let num="33"
+let num1=Number(num)
+console.log( num)
+let number=1
+let ans=Boolean(num)
+console.log(ans)
+let t=33
+let st=String(t)
+console.log(typeof st)
