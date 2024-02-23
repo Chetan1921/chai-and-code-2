20a043c71156182e33516d6353e106f447c779f6
Chetan1921 <149550764+Chetan1921@users.noreply.github.com>
2024-02-23 23:31:47 +0000

    number and math

---
+++ js/07_num_math.js
@@ -0,0 +1,24 @@
+let num=100
+console.log(typeof num)
+let num1=new Number(100)
+console.log(num1)//NUMBER:100
+console.log(typeof num1)//object
+let num2=4.33
+console.log(Math.round(4.33))//
+
+console.log(Math.abs(-4))
+console.log(Math.floor(4.8))
+console.log(Math.ceil(4.66))
+console.log(Math.min(2,1,3,4,0))
+console.log(Math.random())
+let min=10
+let max=20
+/// find value btw 10,20 every time
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+let ans=10000
+let t=ans.toLocaleString(ans)
+console.log(typeof t)
+let r=123.3213
+console.log(r.toPrecision(5))
+console.log(r.toFixed(2))//123.32
+
